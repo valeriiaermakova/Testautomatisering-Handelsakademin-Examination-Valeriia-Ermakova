4f7adbbe64104acb22503baa9135a47ab356fd62
Valeriia Ermakova <valeriia.sverige@gmail.com>
2025-12-02 20:13:09 +0100

    Added test id for catalog button

--- src/pages/catalog_page.py
+++ src/pages/catalog_page.py
@@ -8,6 +8,9 @@ class CatalogPage:
     def navigate(self):
         base_url.get_to_base_url(self.page)
 
+    def get_test_id_for_catalog(self):
+        return self.page.get_by_test_id("catalog")
+
     def click_on_favorite_icon(self, bookTestId):
         self.page.get_by_test_id(bookTestId).click()
 

da735dfd53f3bafc585540afda060c3ba72d886a
Valeriia Ermakova <valeriia.sverige@gmail.com>
2025-12-02 19:47:43 +0100

    Added new scenarios

--- src/steps/step_favorite_books_list.py
+++ src/steps/step_favorite_books_list.py
@@ -1,6 +1,7 @@
 from behave import given, when, then
 from playwright.sync_api import expect
 from pages.catalog_page import CatalogPage
+from pages.my_books_page import MyBooksPage
 
 
 def context_go_to_catalog_page(context):
@@ -42,26 +43,77 @@ def another_book_is_marked(context, book2):
     check_book_is_marked(context, book2)
 
 
-@when("jag tryck på favorit ikon nära en märkt bok")
-def unmark_book(context):
+@when("jag tryck på favorit ikon nära en märkt bok {bookToUnmark}")
+def unmark_book(context, bookToUnmark):
     context_go_to_catalog_page(context)
-    locator = context.web_page.get_book_by_test_id(
-        "star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"
-    )
-    context.web_page.click_on_favorite_icon(
-        "star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"
-    )
+    locator = context.web_page.get_book_by_test_id(bookToUnmark)
+    context.web_page.click_on_favorite_icon(bookToUnmark)
     expect(locator).to_contain_class("selected")
     expect(locator).to_be_visible()
-    context.web_page.click_on_favorite_icon(
-        "star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"
-    )
+    context.web_page.click_on_favorite_icon(bookToUnmark)
 
 
-@then("bok blir märktlös")
-def book_is_unmarked(context):
-    locator = context.web_page.get_book_by_test_id(
-        "star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen"
-    )
+@then("bok blir märktlös {bookToUnmark}")
+def book_is_unmarked(context, bookToUnmark):
+    locator = context.web_page.get_book_by_test_id(bookToUnmark)
     expect(locator).not_to_have_class("selected")
     expect(locator).to_be_visible()
+
+
+@when("jag tryck på en favorit ikon nära en märkt bok {bookToReMark}")
+def unmark_book2(context, bookToReMark):
+    context_go_to_catalog_page(context)
+    context.web_page.click_on_favorite_icon(bookToReMark)
+    context.web_page.click_on_favorite_icon(bookToReMark)
+
+
+@then("boken blir märktlös {bookToReMark}")
+def book_is_unmarked_2(context, bookToReMark):
+    locator = context.web_page.get_book_by_test_id(bookToReMark)
+    expect(locator).not_to_have_class("selected")
+
+
+@then("jag tryck på favorit ikon nära avmarkerade boken {bookToReMark}")
+def mark_book_again_after_unmarking(context, bookToReMark):
+    context.web_page.click_on_favorite_icon(bookToReMark)
+
+
+@then("boken blir märkt {bookToReMark}")
+def book_is_marked_repeatedly(context, bookToReMark):
+    check_book_is_marked(context, bookToReMark)
+
+
+@when('jag kollar upp "Mina böcker" sida innan jag lägger till böcker i lista')
+def go_to_my_books(context):
+    context_go_to_catalog_page(context)
+    context.page.get_by_test_id("favorites").click()
+
+
+@then('jar ser text "När du valt, kommer dina favoritböcker att visas här."')
+def check_empty_my_books_list(context):
+    my_books_page = MyBooksPage(context.page)
+    expect(
+        context.page.get_by_text(my_books_page.expected_text_for_empty_list)
+    ).to_be_visible()
+
+
+@when("jag tryck på favorit ikon nära en marklös bok {book}")
+def mark_favorite_book(context, book):
+    mark_book(context, book)
+
+
+@when('jag går till "Mina böcker"')
+def go_to_my_books_list(context):
+    context.page.get_by_test_id("favorites").click()
+
+
+@then("märkta boken {book} är i mina böcker lista")
+def check_marked_book_in_my_list(context, book):
+    frame = context.page.get_by_test_id("book-list")
+    books = frame.all_text_contents()
+    book_found = False
+    for element in books:
+        if element in book:
+            book_found = True
+            break
+    assert book_found

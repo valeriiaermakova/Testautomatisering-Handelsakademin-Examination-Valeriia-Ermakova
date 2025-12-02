be0e57293ce0bce39fc63f8d7ee3a051165bcde9
Valeriia Ermakova <valeriia.sverige@gmail.com>
2025-12-02 19:47:29 +0100

    Added expected text for empty list

--- src/pages/my_books_page.py
+++ src/pages/my_books_page.py
@@ -4,6 +4,9 @@ import base_url
 class MyBooksPage:
     def __init__(self, page):
         self.page = page
+        self.expected_text_for_empty_list = (
+            " När du valt, kommer dina favoritböcker att visas här. "
+        )
 
     def navigate(self):
         base_url.get_to_base_url(self.page)

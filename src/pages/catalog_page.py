f5bbbe0357ba461848482761d50991abdbadc6cb
Valeriia Ermakova <valeriia.sverige@gmail.com>
2025-12-02 18:19:16 +0100

    Added functions for getting book test id and marking book as favorite

--- src/pages/catalog_page.py
+++ src/pages/catalog_page.py
@@ -6,5 +6,10 @@ class CatalogPage:
         self.page = page
 
     def navigate(self):
-        base_url.get_to_base_url()
-        self.page.get_by_test_id("catalog").click()
+        base_url.get_to_base_url(self.page)
+
+    def mark_book_as_favorite(self, bookTestId):
+        self.page.get_by_test_id(bookTestId).click()
+
+    def get_book_by_test_id(self, bookTestId):
+        return self.page.get_by_test_id(bookTestId)
